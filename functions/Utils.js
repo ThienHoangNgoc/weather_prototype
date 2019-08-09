ac31c7ce62e69b2d69ec8bef65c26b4f75d2bb4c
Thien Hoang Ngoc <ngoc@tbo.de>
2019-08-09 17:13:29 +0200

    added new utils  123

--- functions/Utils.js
+++ functions/Utils.js
@@ -1,17 +1,28 @@
 "use strict";
 //get random Number within the Range of max
-const getRandomInt = (max)=>{
+const getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
 };
 
 
 //compare 2 Strings
-const compareString = (firstString, secondString)=>{
-    return firstString.toString().trim() === secondString;
+const compareString = (firstString, secondString) => {
+    if (checkTypeOf(firstString, 'string') && checkTypeOf(secondString, 'string')) {
+        return firstString.toString().trim() === secondString;
+    }
+    return false;
 };
 
 const checkTypeOf = (object, typeAsString) => {
     return (typeof object === typeAsString);
-}
+};
 
-module.exports = {getRandomInt, compareString, checkTypeOf};
+const containsString = (firstString, secondString) => {
+    if (checkTypeOf(firstString, 'string') && checkTypeOf(secondString, 'string')) {
+        return firstString.includes(secondString);
+    }
+    return false;
+
+};
+
+module.exports = {getRandomInt, compareString, checkTypeOf, containsString};
