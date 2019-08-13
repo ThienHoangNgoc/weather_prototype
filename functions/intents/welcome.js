60dfe7291f31c533a3ebdd0c7cc6322e0d508a1e
Thien Hoang <Legendflashx3@gmail.com>
2019-08-13 13:49:18 +0200

    clean up project structure small change

--- functions/intents/welcome.js
+++ functions/intents/welcome.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const responses = require('../jsons/response_strings')
+const responses = require('../jsons/response_strings');
 
 function welcome(agent) {
     agent.add(responses.welcome.welcome);
