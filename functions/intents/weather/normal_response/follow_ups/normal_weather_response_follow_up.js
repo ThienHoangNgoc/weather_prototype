95d057540edcc331ff048091025a890a0a139158
Thien Hoang <Legendflashx3@gmail.com>
2019-08-22 03:19:22 +0200

    add followup

---
+++ functions/intents/weather/normal_response/follow_ups/normal_weather_response_follow_up.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const normalWeatherResponseFollowUp = (agent) => {
+    let conv = agent.conv();
+
+    agent.add(conv);
+
+
+};
+
+
+module.exports = normalWeatherResponseFollowUp;
